54b729a239a919a866aeda2992f2e457578112dd
vivianp89 <paul.vivian@live.com>
2016-10-09 11:32:23 +0530

    updated

--- deploy.php
+++ deploy.php
@@ -9,8 +9,13 @@
 
 	// The commands
 	$commands = array(
+		'echo $PWD',
 		'whoami',
 		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
 		
 	);
 
@@ -19,7 +24,6 @@
 	foreach($commands AS $command){
 		// Run it
 		$tmp = shell_exec($command);
-		echo $tmp;
 		// Output
 		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
